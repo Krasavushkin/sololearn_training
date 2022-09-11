d5443915a29d12763cd4ed0feb37c3f4ad3539a0
Artem Savushkin <artemkasavushkin@yandex.ru>
2022-09-11 15:55:42 +0300

    new lessons 11.09

---
+++ GameOver.py
@@ -0,0 +1,13 @@
+class Player:
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+
+    def intro(self):
+        print(self.name + " (Level " + self.level + ")")
+
+gamer = input ()
+lvl = input ()
+
+play = Player (gamer, lvl)
+play.intro ()
