95e4bb4a23a84b72251b663bf68788932eac282e
Artem Savushkin <artemkasavushkin@yandex.ru>
2022-09-25 11:48:34 +0300

    new lessons 25.09

---
+++ no dice? no problem.py
@@ -0,0 +1,10 @@
+import random
+random.seed(int(input()))
+
+#сгенерируйте случайное значение для каждого кубика
+
+dice1 = random.randint (1,6)
+dice2 = random.randint (1,6)
+
+print(dice1)
+print(dice2)
